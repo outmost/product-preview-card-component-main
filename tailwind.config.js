4590fcb84083bd872aff95f8c6e1fca824e8d7ca
Tom Sutton <tomsutton1984@gmail.com>
2022-07-12 16:14:40 +1200

    Use correct colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,11 @@ module.exports = {
         transparent: 'transparent',
         black: '#000',
         white: '#fff',
-        cream: '30 38% 92%',
-        cyan: {
-          900: '158 36% 37%',
-        },
+        cream: 'hsl(30, 38%, 92%)',
+        darkcyan: 'hsl(158, 36%, 37%)',
+        verydarkcyan: 'hsl(158, 36%, 17%)',
+        verydarkblue: 'hsl(212, 21%, 14%)',
+        darkgrayblue: 'hsl(228, 12%, 48%)',
       },
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
